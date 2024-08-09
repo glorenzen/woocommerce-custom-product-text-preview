4e7c25f2526ceb70f1fbed65d9c4a80d8a0d7229
Greg Lorenzen <gregorylorenzen@gmail.com>
2024-08-09 12:29:57 -0700

    Initial commit

---
+++ custom-product-text-preview.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: Custom Product Text Preview
+Description: Allows users to customize text on WooCommerce products and preview it before purchase.
+Version: 1.0.0
+Author: Greg Lorenzen
+*/
