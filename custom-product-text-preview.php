325cd4399cd2b0545c0d8b4e06936d4cf0b7947a
Greg Lorenzen <gregorylorenzen@gmail.com>
2024-08-12 15:54:23 -0700

    Add advanced-custom-fields plugin as a requirement

--- custom-product-text-preview.php
+++ custom-product-text-preview.php
@@ -4,6 +4,7 @@ Plugin Name: Custom Product Text Preview
 Description: Allows users to customize text on WooCommerce products and preview it before purchase.
 Version: 1.0.0
 Author: Greg Lorenzen
+Requires Plugins: advanced-custom-fields
 */
 
 // Exit if accessed directly
