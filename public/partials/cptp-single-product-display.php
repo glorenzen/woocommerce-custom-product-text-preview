cd12859bf86cf9acbaf81787378f748ca06911a8
Greg Lorenzen <gregorylorenzen@gmail.com>
2024-08-17 21:33:52 -0700

    Add maxLength attribute to custom text input field

--- public/partials/cptp-single-product-display.php
+++ public/partials/cptp-single-product-display.php
@@ -5,7 +5,7 @@ if (get_field('has_custom_text')) {
     <div class="cptp-product-custom-text" style="display: none;">
         <div class="cptp-input-wrapper">
             <label for="cptp-custom_text" class="cptp-form-label">Custom Text:</label>
-            <input type="text" id="cptp-custom-text" name="cptp-custom_text" class="cptp-form-control" value="" />
+            <input type="text" id="cptp-custom-text" name="cptp-custom_text" class="cptp-form-control" value="" maxLength="20" />
         </div>  
         <div class="cptp-input-wrapper">
             <button id="cptp-preview-text-button" class="cptp-button">Preview Text</button>
