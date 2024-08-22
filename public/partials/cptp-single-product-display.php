a6f1f42b3822f473a6f1abaa78b1c9b36666bda1
Greg Lorenzen <gregorylorenzen@gmail.com>
2024-08-21 22:48:57 -0700

    Add name text preview field and preview button

--- public/partials/cptp-single-product-display.php
+++ public/partials/cptp-single-product-display.php
@@ -3,14 +3,22 @@
 if (get_field('has_custom_text')) {
     // Retrieve the max length for the custom text input from the admin settings
     $custom_text_max_length = get_option('cptp_custom_text_max_length', 20); // Default to 20 if not set
+    $name_text_max_length = get_option('cptp_name_text_max_length', 20); // Default to 20 if not set
     ?>
     <div class="cptp-product-custom-text" style="display: none;">
         <div class="cptp-input-wrapper">
-            <label for="cptp-custom_text" class="cptp-form-label">Custom Text:</label>
-            <input type="text" id="cptp-custom-text" name="cptp-custom_text" class="cptp-form-control" value="" maxLength="<?php echo esc_attr($custom_text_max_length); ?>" />
+            <label for="cptp-name_text" class="cptp-form-label">Name Text</label>
+            <input type="text" id="cptp-custom-name-text" name="cptp-name_text" class="cptp-form-control" value="" maxLength="<?php echo esc_attr($name_text_max_length); ?>" />
+        </div>
+        <div class="cptp-input-wrapper">
+            <button id="cptp-preview-name-text-button" class="cptp-button">Preview</button>
+        </div>
+        <div class="cptp-input-wrapper">
+            <label for="cptp-custom_text" class="cptp-form-label">City Text</label>
+            <input type="text" id="cptp-custom-city-text" name="cptp-custom_text" class="cptp-form-control" value="" maxLength="<?php echo esc_attr($custom_text_max_length); ?>" />
         </div>  
         <div class="cptp-input-wrapper">
-            <button id="cptp-preview-text-button" class="cptp-button">Preview Text</button>
+            <button id="cptp-preview-city-text-button" class="cptp-button">Preview</button>
         </div>
     </div>
 
